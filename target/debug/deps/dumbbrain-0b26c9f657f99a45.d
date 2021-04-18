e0742db242dbdfdfa2cd391228ff0ae8b67940c9
onContentStop <kylecoffey1999@gmail.com>
2021-04-18 18:02:48 -0500

    wups

--- target/debug/deps/dumbbrain-0b26c9f657f99a45.d
+++
@@ -1,5 +0,0 @@
-/home/kyle2/git/dumbbrain-lang/target/debug/deps/dumbbrain-0b26c9f657f99a45.rmeta: dumbbrain/src/lib.rs
-
-/home/kyle2/git/dumbbrain-lang/target/debug/deps/dumbbrain-0b26c9f657f99a45.d: dumbbrain/src/lib.rs
-
-dumbbrain/src/lib.rs:
--- target/debug/deps/dumbbrain-fb63864a723c28c5.d
+++
@@ -1,5 +0,0 @@
-/home/kyle2/git/dumbbrain-lang/target/debug/deps/dumbbrain-fb63864a723c28c5.rmeta: dumbbrain/src/lib.rs
-
-/home/kyle2/git/dumbbrain-lang/target/debug/deps/dumbbrain-fb63864a723c28c5.d: dumbbrain/src/lib.rs
-
-dumbbrain/src/lib.rs:
